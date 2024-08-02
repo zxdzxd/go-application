8fcf460d506bc1524e8dfa401b75957ed424cb80
zxdzxd <iamankashyap15@gmail.com>
2024-08-02 20:52:01 +0530

    done

--- main.go
+++ main.go
@@ -9,23 +9,20 @@ import (
 
 func main() {
 	fmt.Println("Hi gopher!!")
-	yy, mm, dd := time.Now().Date()
-	print(yy, mm, dd)
-	fmt.Println("enter your Name : ")
+	fmt.Print("enter your Name : ")
 	var name string
 	fmt.Scanln(&name)
 	name = strings.TrimSpace(name)
-	fmt.Printf("hi %s !! Please enter Your date of birth\n", name)
-	fmt.Printf("DD/MM/YYYY - ")
+	fmt.Printf("hi %s !! Please enter your date of birth in below format\n", name)
+	fmt.Printf("DD MM YYYY - ")
 	var date, month, year int
 	var err error
-	// fmt.Scanln(&date,"/",&month,"/",&year)
 	n, err := fmt.Scanf("%d %d %d", &date, &month, &year)
 	if n != 3 {
 		err = fmt.Errorf("please enter dob in this format DD MM YYYY ")
 	}
 	if month > 12 || month < 1 {
-		err = fmt.Errorf("please enter correct month %d ",month)
+		err = fmt.Errorf("please enter correct month %d ", month)
 	}
 	if year > 2024 || year < 1920 {
 		err = fmt.Errorf("please enter correct year %d ", year)
@@ -34,12 +31,22 @@ func main() {
 		err = fmt.Errorf("please enter correct date %d ", date)
 	}
 	if err != nil {
-		fmt.Printf("err - %s", err)
-		panic(err)
+		fmt.Printf("err - %s\n", err)
+		return
 	}
 
 	fmt.Printf("hey %s your DOB is - %d/%d/%d\n", name, date, month, year)
-
-	
+	age := time.Now().Year() - year
+	if age == 0 {
+		age = int(time.Now().Month()) - month
+		if age == 0 {
+			age = time.Now().Day() - date
+			fmt.Printf("you are %d day old\n", age)
+			return
+		}
+		fmt.Printf("you are %d month old\n", age)
+		return
+	}
+	fmt.Printf("you are %d years old\n", age)
 
 }
