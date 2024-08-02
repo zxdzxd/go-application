73b477110fde6823339564c7d6b87247751b4776
zxdzxd <iamankashyap15@gmail.com>
2024-08-02 16:25:39 +0530

    dob

--- main.go
+++ main.go
@@ -1,12 +1,45 @@
 // building a simple CLI APP
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"time"
+)
 
 func main() {
 	fmt.Println("Hi gopher!!")
-	fmt.Print("enter your Name")
+	yy, mm, dd := time.Now().Date()
+	print(yy, mm, dd)
+	fmt.Println("enter your Name : ")
 	var name string
 	fmt.Scanln(&name)
-}
+	name = strings.TrimSpace(name)
+	fmt.Printf("hi %s !! Please enter Your date of birth\n", name)
+	fmt.Printf("DD/MM/YYYY - ")
+	var date, month, year int
+	var err error
+	// fmt.Scanln(&date,"/",&month,"/",&year)
+	n, err := fmt.Scanf("%d %d %d", &date, &month, &year)
+	if n != 3 {
+		err = fmt.Errorf("please enter dob in this format DD MM YYYY ")
+	}
+	if month > 12 || month < 1 {
+		err = fmt.Errorf("please enter correct month %d ",month)
+	}
+	if year > 2024 || year < 1920 {
+		err = fmt.Errorf("please enter correct year %d ", year)
+	}
+	if date > 31 || date < 1 {
+		err = fmt.Errorf("please enter correct date %d ", date)
+	}
+	if err != nil {
+		fmt.Printf("err - %s", err)
+		panic(err)
+	}
 
+	fmt.Printf("hey %s your DOB is - %d/%d/%d\n", name, date, month, year)
+
+	
+
+}
