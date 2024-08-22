3839213085bbc25c2243269a8ad04d8819b0b7d7
zxdzxd <iamankashyap15@gmail.com>
2024-08-22 13:00:18 +0530

    word count command using flag

--- wordcount/wc.go
+++ wordcount/wc.go
@@ -6,16 +6,24 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
+	"unicode"
 )
 
 var (
-	bytecount *string
-	filename  *string
+	bytecount  *string
+	filename   *string
+	total_line *string
+	totalword  *string
+	totalchar  *string
 )
 
 func init() {
 	bytecount = flag.String("c", "", "no of bytes in a file")
 	filename = flag.String("f", "", "file name")
+	total_line = flag.String("l", "", "no of lines in a file")
+	totalword = flag.String("w", "", "no of words in a file")
+	totalchar = flag.String("m", "", "no of characters in a file")
 }
 
 func Wordcount() {
@@ -23,19 +31,48 @@ func Wordcount() {
 	flag.Parse()
 	// *filename = "wordcount/test.txt"
 	if *filename == "" {
-		fmt.Printf("Input file name using -f flag")
+		fmt.Println("Input file name using -f flag")
 	}
 	data, err := os.ReadFile(*filename)
+	datastring := string(data)
 	if err != nil {
 		fmt.Printf("unable to read file \nError- %s", err)
 		return
 	}
-	
-
-	if *bytecount == "c" {
+	switch {
+	case *bytecount == "c":
 		fmt.Println(len(data))
-	} else {
-		fmt.Printf("count %s\n", *bytecount)
+
+	case *total_line == "l":
+		sliceofstring := strings.Split(datastring, "\n")
+
+		fmt.Println(len(sliceofstring) - 1)
+
+	case *totalword == "w":
+		// fmt.Println(len(datastring))
+		word_count := 0
+		foundSpace := false
+		for _, j := range datastring {
+			if unicode.IsSpace(j) {
+				foundSpace = false
+			} else if !foundSpace {
+				foundSpace = true
+				word_count++
+			}
+		}
+		fmt.Println(word_count)
+	case *totalchar == "m":
+		// char_count := 0
+		total_char := strings.Split(datastring, "")
+
+		// for _, j := range datastring {
+			
+		// 	// if unicode.IsSpace(j) {
+		// 	// 	char_count++
+		// 	// }
+		// }
+		fmt.Println(len(total_char))
+
 	}
 
 }
